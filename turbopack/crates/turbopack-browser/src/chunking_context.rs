9b5bdb9a97c099a3dd31eb28d2f19723d35a9f2a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-03-03 16:32:32 +0100

    WIP proper chunking

--- turbopack/crates/turbopack-browser/src/chunking_context.rs
+++ turbopack/crates/turbopack-browser/src/chunking_context.rs
@@ -714,7 +714,6 @@ impl ChunkingContext for BrowserChunkingContext {
         let span = tracing::info_span!("chunking", name = display(ident.to_string().await?));
         async move {
             let this = self.await?;
-            let entries = chunk_group.entries();
             let input_availability_info = availability_info;
             let MakeChunkGroupResult {
                 chunks,
@@ -722,7 +721,7 @@ impl ChunkingContext for BrowserChunkingContext {
                 references,
                 availability_info,
             } = make_chunk_group(
-                entries,
+                &chunk_group,
                 module_graph,
                 ResolvedVc::upcast(self),
                 input_availability_info,
@@ -783,14 +782,13 @@ impl ChunkingContext for BrowserChunkingContext {
         );
         async move {
             let this = self.await?;
-            let entries = chunk_group.entries();
             let MakeChunkGroupResult {
                 chunks,
                 referenced_output_assets,
                 references,
                 availability_info,
             } = make_chunk_group(
-                entries,
+                &chunk_group,
                 module_graph,
                 ResolvedVc::upcast(self),
                 input_availability_info,
